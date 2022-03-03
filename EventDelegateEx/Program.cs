5834dc3915ac0a839e4e88fbf92f49f7d9640184
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-03-03 11:32:23 +0530

    Add project files.

---
+++ EventDelegateEx/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartProgram sp = new StartProgram();
+            sp.completedProcess += Sp_completedProcess;
+            sp.StartProcess();
+        }
+
+        private static void Sp_completedProcess()
+        {
+            Console.WriteLine("Welocme to the World of friends");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EventDelegateEx/StartProgram.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDelegateEx
+{
+    public delegate void Start();
+    class StartProgram
+    {
+
+        public event Start completedProcess;
+           
+        public void StartProcess()
+        {
+           
+            
+            Console.WriteLine("Process started");
+            Console.ReadLine();
+            OncompletedProcess();
+        }
+
+        private void OncompletedProcess()
+        {
+            completedProcess?.Invoke();
+        }
+    }
+}
